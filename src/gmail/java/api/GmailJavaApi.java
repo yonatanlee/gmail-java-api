519e70142970c99b20e1ec35a468e4ccc82db318
Anantha Krishnan <ananthakrishnan.s.r@gmail.com>
2012-05-09 16:29:56 +0530

    Created new Netbeans Project

---
+++ src/gmail/java/api/GmailJavaApi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gmail.java.api;
+
+/**
+ *
+ * @author anantha
+ */
+public class GmailJavaApi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
